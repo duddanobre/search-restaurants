c511300aae12bcdcb59eea7c9099c222da80e8bc
Maria Eduarda Nobre <dudarnobre@gmail.com>
2025-07-26 11:25:55 -0300

    page initial

--- index.py
+++ index.py
@@ -1,8 +1,31 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+
+st.sidebar.page_link("index.py", label="🏠 Home")
+st.sidebar.page_link("pages/analise.py", label="📊 Análise")
+st.sidebar.page_link("pages/search.py", label="🔍 Buscar restaurante")
+
+autores = ["front: Maria Eduarda Roxa Nobre", "dash/metricas: Denise Ramos Soares", "dados: Ana Caroline Vieira Amorim", "engenharia: Carlos Eduardo Oliveira Martins"]
+
+with st.sidebar:
+    st.markdown(
+        """
+        <style>
+            .sidebar .sidebar-content {
+                background-color: #f0f2f5;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    "Autores:" 
+
+    for autor in autores:
+        st.markdown(f"- {autor}")
 
 
-st.title('Busca restaurantes')
+st.title("Restaurantes em Fortaleza")
 
+st.header("Sobre", divider=True)
 
+st.text_area("Este é um projeto de análise de dados de restaurantes em Fortaleza, Ceará, Brasil. O objetivo é fornecer uma visão geral dos estabelecimentos, incluindo informações sobre localização, tipo de cozinha e avaliações dos clientes. A análise é baseada em dados coletados de fontes públicas e visa ajudar os usuários a encontrar o restaurante ideal para suas necessidades.")

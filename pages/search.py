6ae97801083144f3bbd765aaf646ee1f92936936
Maria Eduarda Nobre <dudarnobre@gmail.com>
2025-07-26 11:46:33 -0300

    search page

---
+++ pages/search.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🔍 Buscar Restaurante")
+
+# restaurante mais proximo, menor tempo de espera e melhor avaliação
+
+st.markdown("### Use o campo abaixo para buscar por nome ou tipo de cozinha")
